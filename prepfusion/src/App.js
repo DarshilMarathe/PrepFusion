763fc781f8deff9f388c823d2fd759ea1c089ef0
Darshil Marathe <99969644+DarshilMarathe@users.noreply.github.com>
2023-10-01 20:36:33 +0530

    Skeleton for UserDashboard

--- prepfusion/src/App.js
+++ prepfusion/src/App.js
@@ -8,6 +8,8 @@ import Mlpredictor from './pages/mlpredictor/Mlpredictor';
 import  Problemset from './pages/problemset/Problemset';
 import Login from './pages/login/Login'
 import Signin from './pages/signin/Signin'
+import Tester from './pages/darshil_test/Tester';
+import UserDashboard from './pages/UserDashboard/UserDashboard';
 
 
 
@@ -22,6 +24,8 @@ function App() {
       <Route path='/signup' element = {<Signin/>} />
       <Route path='/problemset' element = {<Problemset/>} />
       <Route path='/upload' element = {<Mlpredictor/>} />
+      <Route path='/tester' element = {<Tester/>} />
+      <Route path='/user' element = {<UserDashboard/>} />
     </Routes>
 
    <Footer/>
