e771445477e12a27dcd064899682a358170f8cd9
Rutvik Kaslay <rvkaslay@gmail.com>
2023-10-02 19:36:55 +0530

    dashboard changes done

--- prepfusion/src/pages/UserDashboard/UserDashboard.js
+++ prepfusion/src/pages/UserDashboard/UserDashboard.js
@@ -1,43 +1,52 @@
-import React,{useEffect,useState} from 'react'
-import {useNavigate} from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import './UserDashboard.css'
+import "./UserDashboard.css";
 
 export default function UserDashboard() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [userData, setUserData] = useState({name:"",email:"",ispremium:"",chatbot:""})
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    ispremium: "",
+    chatbot: "",
+  });
 
-    useEffect(() => {
+  // useEffect(() => {
 
-        if(localStorage.getItem("token")){
-          userDetails();
-        }
-        else{
-          alert("Login to Continue")
-          navigate('/login')
-        }
-          //eslint-disable-next-line
-      }, [])
-    
-    const userDetails=async ()=>{
-          const response = await fetch(`http://localhost:5000/auth/getuser`, {
-              method: "POST", 
-              headers: {
-                "Content-Type": "application/json",
-                "auth-token" : localStorage.getItem("token")            
-              },
-            });
-            let json =  await response.json();
-            console.log(json);
-            if(json.success){
-                setUserData({name:json.user.name,email:json.user.email,ispremium:json.user.isPremium,chatbot:json.user.chatbot})
-            }
-            else{
-              alert("Invalid Credentials")
-              navigate('/');
-            }
+  //     if(localStorage.getItem("token")){
+  //       userDetails();
+  //     }
+  //     else{
+  //       alert("Login to Continue")
+  //       navigate('/login')
+  //     }
+  //       //eslint-disable-next-line
+  //   }, [])
+
+  const userDetails = async () => {
+    const response = await fetch(`http://localhost:5000/auth/getuser`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": localStorage.getItem("token"),
+      },
+    });
+    let json = await response.json();
+    console.log(json);
+    if (json.success) {
+      setUserData({
+        name: json.user.name,
+        email: json.user.email,
+        ispremium: json.user.isPremium,
+        chatbot: json.user.chatbot,
+      });
+    } else {
+      alert("Invalid Credentials");
+      navigate("/");
     }
+  };
   // <script>
   //     // Add event listeners to the tabs
   //     document.querySelectorAll(".db-tab").forEach((tab) => {
@@ -60,119 +69,103 @@ export default function UserDashboard() {
   //   </script>
   return (
     <div className="db-main">
-      <div className="db-left-div">
-        <div className="db-profile-pic">RK</div>
-        <p>Rutvik</p>
-        <div className="db-tabs">
-          <a className="db-tab db-tab-active" href="#">
-            Profile
-          </a>
-          <a className="db-tab" href="#">
-            Subscriptions
-          </a>
-          <a className="db-tab" href="#">
-            Bookmarks
-          </a>
-        </div>
-      </div>
-      <div className="db-dummy" />
-      <div className="db-right-div">
-        <div className="db-profile" id="profile-section">
-          <p className="db-section-heading">My profile</p>
-          <div className="db-details">
-            <p className="db-details-subheading">Your details</p>
-            <form action>
-              <input
-                className="db-username"
-                type="text"
-                placeholder="First name"
-              />
-              <input
-                className="db-password"
-                type="text"
-                placeholder="Last name"
-              />
-              <input
-                className="db-password"
-                type="text"
-                placeholder="Username"
-              />
-              <input className="db-password" type="text" placeholder="Email" />
-              <button className="db-submit-button">Save</button>
-            </form>
+        <div className="db-left-div">
+          <div className="db-profile-pic">RK</div>
+          <p>Rutvik</p>
+          <div className="db-tabs">
+            <a className="db-tab db-tab-active" href="#">
+              Profile
+            </a>
+            <a className="db-tab" href="#">
+              Subscriptions
+            </a>
+            <a className="db-tab" href="#">
+              Bookmarks
+            </a>
           </div>
         </div>
-        <div className="db-subcription" id="subscriptions-section">
-          <p className="db-section-heading">Your Subscription</p>
-          <div className="db-plans">
-            <div className="db-free">
-              <img src alt="target" />
-              <p className="db-plan-heading">Free</p>
-              <p className="db-plan-subheading">
-                Rs. 0 <span>per month</span>
-              </p>
-              <hr />
-              <div className="db-features">
-                <p>Chatbot (3 trials )</p>
-                <p>QClass</p>
-                <p>PYQ's</p>
-                <p>Ads</p>
+        <div className="db-dummy" />
+        <div className="db-right-div">
+          <div className="db-profile" id="profile-section">
+            <p className="db-section-heading">My profile</p>
+            <div className="db-details">
+              <p className="db-details-subheading">Your details</p>
+              <div>
+                <div className="db-username"> firstname : </div>
+                <div className="db-username"> lastname :</div>
+                <div className="db-username"> username :</div>
+                <div className="db-username"> Email :</div>
               </div>
-              <button>Get started</button>
-            </div>
-            <div className="db-paid">
-              <img src alt="target" />
-              <p className="db-plan-heading">Premium</p>
-              <p className="db-plan-subheading">
-                Rs. 199 <span>per month</span>
-              </p>
-              <hr />
-              <div className="db-features">
-                <p>Chatbot</p>
-                <p>QClass</p>
-                <p>PYQ's</p>
-                <p>FAQ's</p>
-              </div>
-              <button>Get started</button>
             </div>
           </div>
-        </div>
-        <div className="db-bookmark" id="bookmarks-section">
-          <p className="db-section-heading">Your Bookmark's</p>
-          <div className="db-plans">
-            <div className="db-free">
-              <img src alt="target" />
-              <p className="db-plan-heading">Free</p>
-              <p className="db-plan-subheading">
-                Rs. 0 <span>per month</span>
-              </p>
-              <hr />
-              <div className="db-features">
-                <p>Chatbot (3 trials )</p>
-                <p>QClass</p>
-                <p>PYQ's</p>
-                <p>Ads</p>
+          <div className="db-subcription" id="subscriptions-section">
+            <p className="db-section-heading">Your Subscription</p>
+            <div className="db-plans">
+              <div className="db-free">
+                <p className="db-plan-heading">Free</p>
+                <p className="db-plan-subheading">
+                  Rs. 0 <span>per month</span>
+                </p>
+                <hr />
+                <div className="db-features">
+                  <p>Chatbot (3 trials )</p>
+                  <p>QClass</p>
+                  <p>PYQ's</p>
+                  <p>Ads</p>
+                </div>
+                <button>Get started</button>
               </div>
-              <button>Get started</button>
+              <div className="db-paid">
+                <p className="db-plan-heading">Premium</p>
+                <p className="db-plan-subheading">
+                  Rs. 199 <span>per month</span>
+                </p>
+                <hr />
+                <div className="db-features">
+                  <p>Chatbot</p>
+                  <p>QClass</p>
+                  <p>PYQ's</p>
+                  <p>FAQ's</p>
+                </div>
+                <button>Get started</button>
+              </div>
             </div>
-            <div className="db-paid">
-              <img src alt="target" />
-              <p className="db-plan-heading">Premium</p>
-              <p className="db-plan-subheading">
-                Rs. 199 <span>per month</span>
-              </p>
-              <hr />
-              <div className="db-features">
-                <p>Chatbot</p>
-                <p>QClass</p>
-                <p>PYQ's</p>
-                <p>FAQ's</p>
+          </div>
+          <div className="db-bookmark" id="bookmarks-section">
+            <p className="db-section-heading">Your Bookmark's</p>
+            <div className="db-plans">
+              <div className="db-free">
+                <p className="db-plan-heading">Free</p>
+                <p className="db-plan-subheading">
+                  Rs. 0 <span>per month</span>
+                </p>
+                <hr />
+                <div className="db-features">
+                  <p>Chatbot (3 trials )</p>
+                  <p>QClass</p>
+                  <p>PYQ's</p>
+                  <p>Ads</p>
+                </div>
+                <button>Get started</button>
               </div>
-              <button>Get started</button>
+              <div className="db-paid">
+                <p className="db-plan-heading">Premium</p>
+                <p className="db-plan-subheading">
+                  Rs. 199 <span>per month</span>
+                </p>
+                <hr />
+                <div className="db-features">
+                  <p>Chatbot</p>
+                  <p>QClass</p>
+                  <p>PYQ's</p>
+                  <p>FAQ's</p>
+                </div>
+                <button>Get started</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+
     </div>
   );
 }
